00c440bd32056455598b1a42af855f72d231b61d
Zjl-u <2373007562@qq.com>
2026-01-28 12:58:26 +0800

    Implement AJAX server with CORS support

--- 代码/3.原生AJAX/server.js
+++ 代码/3.原生AJAX/server.js
@@ -7,9 +7,62 @@ const app = express();
 // 3.创建路由规则
 // request 是对请求报文的封装
 // response是对响应报文的封装
-app.get('/', (request, response) => {
+app.get('/server', (request, response) => {
+    // 设置响应头   设置允许跨越
+    response.setHeader('Access-Control-Allow-Origin', '*')
+
     // 设置响应
-    response.send('HELLO EXPRESS')
+    response.send('HELLO AJAX')
+})
+
+// 可以接受任意类型的请求
+app.all('/server', (request, response) => {
+    // 设置响应头   设置允许跨越
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    // 响应头
+    response.setHeader('Access-Control-Allow-Headers', '*')
+    // 响应一个数据
+    const data = {
+        name: 'cc'
+    }
+    // 对对象进行字符串转换
+    let str = JSON.stringify(data)
+    // 设置响应
+    response.send(str)
+})
+
+// JSON响应
+app.all('/json-server', (request, response) => {
+    // 设置响应头   设置允许跨越
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    // 响应头
+    response.setHeader('Access-Control-Allow-Headers', '*')
+    // 响应一个数据
+    const data = {
+        name: 'cc'
+    }
+    // 对对象进行字符串转换
+    let str = JSON.stringify(data)
+    // 设置响应
+    response.send(str)
+})
+
+// 针对IE缓存
+app.get('/ie', (request, response) => {
+    // 设置响应头   设置允许跨越
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    // 设置响应
+    response.send('hello ie')
+})
+
+// 延时响应
+app.get('/delay', (request, response) => {
+    // 设置响应头   设置允许跨越
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    setTimeout(() => {
+        // 设置响应
+        response.send('hello 延时响应')
+    }, 3000)
 })
 
 // 4.监听端口启动服务
